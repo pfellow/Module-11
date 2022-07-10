6f34c6627abf783364e4d2f7a161b266644df365
sshilovskiy <N+AJ1o@!_)S>
2022-07-10 23:13:05 +0300

    initial

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import styles from "./Cart.module.css";
+
+const Cart = (porps) => {
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={styles.actions}></div>
+      <button className={styles["button--alt"]}>Close</button>
+      <button className={styles.button}>Order</button>
+    </div>
+  );
+};
+
+export default Cart;
