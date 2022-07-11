ce6b1ebeed5667f1443ea2853403760317725789
sshilovskiy <N+AJ1o@!_)S>
2022-07-11 23:59:22 +0300

    144 completed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
+import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
 
-const Cart = (porps) => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={styles["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
@@ -10,16 +11,17 @@ const Cart = (porps) => {
   );
 
   return (
-    <div>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={styles.actions}></div>
-      <button className={styles["button--alt"]}>Close</button>
-      <button className={styles.button}>Order</button>
-    </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]} onClick={props.onClose}>Close</button>
+        <button className={styles.button}>Order</button>
+      </div>
+    </Modal>
   );
 };
 
