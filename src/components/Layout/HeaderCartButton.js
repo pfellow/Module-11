6456acbf9ac2181b59076a693a239edb1d891aa3
sshilovskiy <N+AJ1o@!_)S>
2022-07-13 00:09:04 +0300

    completed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,25 +1,46 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from "react";
 
-import CartIcon from '../Cart/CartIcon';
-import CartContext from '../../store/cart-context';
-import styles from './HeaderCartButton.module.css';
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+import styles from "./HeaderCartButton.module.css";
 
-const HeaderCartButton = props => {
-    const cartCtx = useContext(CartContext);
+const HeaderCartButton = (props) => {
+   const [btnIsHighlighted, setBtnIsHighLIghted] = useState(false);
+    
+  const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-        return curNumber + item.amount;
-    }, 0);
+  const { items } = cartCtx;
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-    return <button className={styles.button} onClick={props.onClick}>
-        <span className={styles.icon}>
-            <CartIcon />
-        </span>
-        <span>Your cart</span>
-        <span className={styles.badge}>
-            {numberOfCartItems}
-        </span>
+
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+        return;
+    }
+    setBtnIsHighLIghted(true);
+
+    const timer = setTimeout(() => {
+        setBtnIsHighLIghted(false);
+    }, 300);
+
+    return () => {
+        clearTimeout(timer);
+    }
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your cart</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
+  );
 };
 
 export default HeaderCartButton;
